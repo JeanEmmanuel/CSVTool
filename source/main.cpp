5a6c87072814a2a4338b5efdf5e8517ce733ec17
Jean-Emmanuel Losi <jean.e.losi@gmail.com>
2017-01-12 12:17:11 -0500

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -4,10 +4,6 @@
  * Creator Jean-Emmanuel Losi, jean.e.losi@gmail.com
  */
 
-// /Users/jean-emmanuellosi/anaconda/pkgs/matplotlib-1.4.3-np19py27_1/lib/python2.7/site-packages/matplotlib/mpl-data/sample_data/aapl.csv
-// /Users/jean-emmanuellosi/Desktop/Investment strategy/Intro to maths fi/75f2a00816031277.csv
-// /Users/jean-emmanuellosi/Downloads/table.csv
-
 #include <iostream>
 #include <string>
 #include "Interface.h"
